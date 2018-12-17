bf48586ede6e16da1cdbca4b99be410f571db9f5
FELINESOFT\simon.crouch <simon.crouch@felinesoft.com>
2018-12-17 15:55:58 +0000

    updated json, testing ok now

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Microsoft-Teams-Connector
 
-Powershell script for sending in information to Microsoft Teams via incoming web-hook, for example, when a build has succeeded. See the [official instructions]("https://docs.microsoft.com/en-us/microsoftteams/platform/concepts/connectors/connectors-using") for how to send more complicated "cards" into MS Teams.
+Powershell script for sending in information to Microsoft Teams via incoming web-hook, customised for an Azure DevOps / TFVC build server. For example, a success message is sent into Teams when a build has succeeded.
 
 ## Preparation
 
@@ -10,16 +10,37 @@ Create a file called `MSTeamsWebhook.ps1` with this content:
 $teamsWebhookUrl = 'https://outlook.office.com/webhook/<myTeamsIncommingWebHookURL>'
 ```
 
-## Running the script
+## Running the script (Azure DevOps / VSTS)
 
-Call this script with:
+Either simply add a build definition step of "Powershell" and point it to `message-send.ps1` (and the file will work out the Azure variables automatically), or call manually with:
 
 ```Powershell
-message-send.ps1 "BuildNumber" "Environment"
+message-send.ps1 "BuildBuildNumber" "BuildDefinitionName" "AGENTJOB_STATUS" "BuildTriggeredBy" "BuildBuildUri"
 ```
 
 e.g.
 
 ```Powershell
-message-send.ps1 "1.2" "PROD"
+message-send.ps1 "1.2" "Test definition" "Succeeded" "API" "http://yahoo.com"
 ```
+
+## Screenshot
+
+Example build success message:
+
+![screenshot](screenshot%20of%20alert.png)
+
+## Script Testing
+
+You can output the card json (which would be sent to Teams) with:
+
+```batch
+Powershell.exe -executionpolicy remotesigned -File message-send.ps1 "1.2" "Test definition" "Succeeded" "API" "http://yahoo.com"
+```
+
+## More reading
+
+* [Legacy actionable message card reference](https://docs.microsoft.com/en-gb/outlook/actionable-messages/message-card-reference) - only card reference which works for MS Teams!!!
+* [Card Design Playground](https://messagecardplayground.azurewebsites.net/)
+* [Build definition variables](https://docs.microsoft.com/en-us/azure/devops/pipelines/build/variables)
+* [Using Connectors]("https://docs.microsoft.com/en-us/microsoftteams/platform/concepts/connectors/connectors-using")
--- message-send.ps1
+++ message-send.ps1
@@ -1,16 +1,53 @@
 Set-StrictMode -Version Latest
 
+function Get-ScriptDirectory
+{
+    Split-Path $script:MyInvocation.MyCommand.Path
+}
+
 # Microsoft Incomming Webhook
 # $teamsWebhookUrl should be included in the below .ps1 file
 . (Get-ScriptDirectory | Join-Path -ChildPath MSTeamsWebhook.ps1)
 
 function Send-Message {
     param (
-        [string]$buildNumber,
-        [string]$environment
+        [string]$BuildBuildNumber,
+        [string]$BuildDefinitionName,
+        [string]$AgentJobStatus,
+        [string]$BuildTriggeredBy,
+        [string]$BuildBuildUri
     )
-    $message = @{"text" = "Hello, build number $buildNumber succeeded under $environment"}
-    Invoke-RestMethod -Method post -ContentType 'Application/Json' -Body (ConvertTo-Json -Compress -InputObject $message) -Uri $teamsWebhookUrl
+    $message = [pscustomobject]@{
+        '@type' = "MessageCard"
+        '@context' = "https://schema.org/extensions"
+        "themeColor" = if ($AgentJobStatus -eq "Succeeded") {"008000"} else {"ff0000"}
+        "title" = "$BuildDefinitionName $BuildBuildNumber`: **$AgentJobStatus**"
+        "text" = "Triggered by $BuildTriggeredBy"
+        "potentialAction" = @(@{
+            '@type' = "OpenUri"
+            "name" = "Open Build Summary"
+            "targets" = @(@{
+                "os" = "default"
+                "uri" = $BuildBuildUri
+            })
+        })
+    }
+    Invoke-RestMethod -Method post -ContentType 'Application/Json' -Body (ConvertTo-Json -Compress -Depth 5 -InputObject $message) -Uri $teamsWebhookUrl
+    # Write-Output -Method post -ContentType 'Application/Json' -Body (ConvertTo-Json -Compress -Depth 5 -InputObject $message) -Uri $teamsWebhookUrl
 }
 
-Send-Message -buildNumber $args[0] -environment $args[1]
+# Azure DevOps Variables
+# https://docs.microsoft.com/en-us/azure/devops/pipelines/build/variables?view=vsts
+$BuildBuildNumber = if ($args[0]) {$args[0]} else {$Env:BUILD_BUILDNUMBER}
+$BuildDefinitionName = if ($args[1]) {$args[1]} else {$env:BUILD_DEFINITIONNAME}
+$AgentJobStatus = if ($args[2]) {$args[2]} else {$env:AGENT_JOBSTATUS}
+$BuildTriggeredBy = if ($args[3]) {$args[3]} else {$env:BUILD_TRIGGEREDBY}
+$BuildBuildUri = if ($args[4]) {$args[4]} else {$env:BUILD_BUILDURI}
+
+# Run
+Send-Message `
+-BuildBuildNumber $BuildBuildNumber `
+-BuildDefinitionName $BuildDefinitionName `
+-AgentJobStatus $AgentJobStatus `
+-BuildTriggeredBy $BuildTriggeredBy `
+-BuildBuildUri $BuildBuildUri
